e6955db21cd165f12aa8b8f9ba7a36539830cce6
imindMan <khanhdonglethanhtong7b2@gmail.com>
2024-08-17 15:24:19 +0700

    Day 7 finished

--- day7/day7_part2.rs
+++ day7/day7_part2.rs
@@ -75,30 +75,62 @@ fn handle_line(line: String) -> (String, HandTypes, i32) {
     for i in arr[0].chars() {
         counters.entry(i).and_modify(|x| *x += 1).or_insert(1);
     }
+
+    let mut counter_five: i32 = 0;
+    let mut counter_four: i32 = 0;
     let mut counter_three: i32 = 0;
     let mut counter_two: i32 = 0;
     let mut counter_one: i32 = 0;
-
+    let mut jokers_counter: i32 = 0;
     for ele in &counters {
-        match *ele.1 {
-            5 => return (arr[0].clone(), HandTypes::FiveOfAKind, rough_value),
-            4 => return (arr[0].clone(), HandTypes::FourOfAKind, rough_value),
-            3 => counter_three += 1,
-            2 => counter_two += 1,
-            1 => counter_one += 1,
-            _ => continue,
+        if *ele.0 != 'J' {
+            match *ele.1 {
+                5 => counter_five += 1,
+                4 => counter_four += 1,
+                3 => counter_three += 1,
+                2 => counter_two += 1,
+                1 => counter_one += 1,
+                _ => continue,
+            }
+        } else {
+            jokers_counter += *ele.1;
         }
     }
-    if counter_three == 1 && counter_two == 1 {
+    if jokers_counter == 0 {
+        if counter_five == 1 {
+            (arr[0].clone(), HandTypes::FiveOfAKind, rough_value)
+        } else if counter_four == 1 {
+            (arr[0].clone(), HandTypes::FourOfAKind, rough_value)
+        } else if counter_three == 1 && counter_two == 1 {
+            (arr[0].clone(), HandTypes::FullHouse, rough_value)
+        } else if counter_three == 1 && counter_one == 2 {
+            (arr[0].clone(), HandTypes::ThreeOfAKind, rough_value)
+        } else if counter_two == 2 {
+            (arr[0].clone(), HandTypes::TwoPair, rough_value)
+        } else if counter_two == 1 {
+            (arr[0].clone(), HandTypes::OnePair, rough_value)
+        } else {
+            (arr[0].clone(), HandTypes::HighCard, rough_value)
+        }
+    } else if counter_five == 1
+        || counter_four == 1
+        || (counter_three == 1 && jokers_counter == 2)
+        || (counter_two == 1 && jokers_counter == 3)
+        || jokers_counter == 5
+        || jokers_counter == 4
+    {
+        (arr[0].clone(), HandTypes::FiveOfAKind, rough_value)
+    } else if (counter_three == 1 && jokers_counter == 1)
+        || (counter_two == 1 && jokers_counter == 2)
+        || jokers_counter == 3
+    {
+        (arr[0].clone(), HandTypes::FourOfAKind, rough_value)
+    } else if counter_two == 2 || (jokers_counter == 3 && counter_two == 1) {
         (arr[0].clone(), HandTypes::FullHouse, rough_value)
-    } else if counter_three == 1 && counter_one == 2 {
+    } else if counter_two == 1 && jokers_counter == 1 || jokers_counter == 2 {
         (arr[0].clone(), HandTypes::ThreeOfAKind, rough_value)
-    } else if counter_two == 2 {
-        (arr[0].clone(), HandTypes::TwoPair, rough_value)
-    } else if counter_two == 1 {
-        (arr[0].clone(), HandTypes::OnePair, rough_value)
     } else {
-        (arr[0].clone(), HandTypes::HighCard, rough_value)
+        (arr[0].clone(), HandTypes::OnePair, rough_value)
     }
 }
 
@@ -112,7 +144,7 @@ where
 
 fn sort(vec_to_be_sort: &mut [(String, i32)]) {
     let order = [
-        '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+        'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
     ];
 
     // Create a hash map for quick lookup of the order index
