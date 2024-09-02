d735efd9a3d5d4df24fd8e6cdd238d37dde525d9
imindMan <khanhdonglethanhtong7b2@gmail.com>
2024-09-02 22:37:41 +0700

    Zig init

--- day12/day12_part1.zig
+++ day12/day12_part1.zig
@@ -14,7 +14,7 @@ pub fn main() !void {
     }
 }
 
-pub fn traceCounter(line: []const u8) !void {
+fn traceCounter(line: []const u8) !void {
     var it = std.mem.split(u8, line, " ");
     var machines: []const u8 = undefined;
     var criterias: []const u8 = undefined;
@@ -31,23 +31,57 @@ pub fn traceCounter(line: []const u8) !void {
     const criterias_final = criteriasMaker(criterias);
 
     std.debug.print("{s}\n", .{machines});
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("{any}\n", .{criterias_final});
+    std.debug.print("{any}\n", .{criterias_final}); 
+    
+    var value: i32 = 0;
+    for (try criterias_final) |chars| {
+        const char_int: i32 = @as(i32, chars);
+        value = move(value, char_int, machines);
+    }
+
+    std.debug.print("{any}\n", .{value});
 }
 
-fn criteriasMaker(criterias: []const u8) ![]i32 {
+fn move(position: i32, chars: i32, line: []const u8) i32 {
+   if (position + chars - 1 >= line.len) {
+        return -1;
+   }
+   var pos_check: u8 = @as(usize, position - 1);
+   // check if there's any prev trailing characters
+   if (pos_check >= 0 and line[pos_check] == '#') {
+       while (position >= 0 and line[position] != '.') {
+          pos_check -= 1;
+       }
+       pos_check += 1;
+   } else {
+        pos_check = @as(usize, position);
+   }
+   for (pos_check..(pos_check + chars - 1)) |index| {
+       if (line[index] == '.') {
+            return -1;
+       }
+   }
+   // check if there's any behind trailing characters
+   if (pos_check + chars < line.len and line[pos_check + chars] == '#') {
+      return -1;
+   }
+   
+   return position + chars;
+}
+
+fn criteriasMaker(criterias: []const u8) ![]u8 {
     var allocator = std.heap.page_allocator;
     var it = std.mem.split(u8, criterias, ",");
     var len:u8 = 0;
     while (it.next() != null) {
         len += 1;
     }
-    var result = try allocator.alloc(i32, len);
+    var result = try allocator.alloc(u8, len);
     it.index = 0;
     // Fill the result array with parsed integers
     var index: usize = 0;
     while (it.next()) |token| {
-        const maybe_num = try std.fmt.parseInt(i32, token, 10);
+        const maybe_num = try std.fmt.parseInt(u8, token, 10);
         result[index] = maybe_num;
         index += 1;
     }
