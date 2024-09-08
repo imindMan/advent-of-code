31ffd551615d2a73a02400157612c950fb06c23f
imindMan <khanhdonglethanhtong7b2@gmail.com>
2024-09-08 13:44:57 +0700

    Shorten crystal code

--- day10/day10_part2.cr
+++ day10/day10_part2.cr
@@ -1,13 +1,3 @@
-# the plan is to use some backtracking to solve
-# | is a vertical pipe connecting north and south.
-# - is a horizontal pipe connecting east and west.
-# L is a 90-degree bend connecting north and east.
-# J is a 90-degree bend connecting north and west.
-# 7 is a 90-degree bend connecting south and west.
-# F is a 90-degree bend connecting south and east.
-# . is ground; there is no pipe in this tile.
-# S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
-
 file = File.read_lines("day10.txt")
 
 def move(instruction, map, x, y)
