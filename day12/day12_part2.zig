3b824f184d87c610a31a473c0a56fe3e09bbb560
imindMan <khanhdonglethanhtong7b2@gmail.com>
2024-11-03 22:49:42 +0700

    Zig update

--- day12/day12_part2.zig
+++ day12/day12_part2.zig
@@ -19,21 +19,33 @@ pub fn main() !void {
 
 fn traceCounter(line: []const u8) !i32 {
     var it = std.mem.split(u8, line, " ");
-    var machines: []const u8 = undefined;
+    var allocator = std.heap.page_allocator;
+
+    var machines: []u8 = undefined;
     var criterias: []const u8 = undefined;
 
     var x: u8 = 0;
     while (it.next()) |data| {
         if (x == 0) {
-            machines = data;
+            machines = try allocator.alloc(u8, data.len * 5 + 5);
+            var literal_index: u8 = 0;
+            for (0..5) |_| {
+                for (0..data.len) |i| {
+                    machines[literal_index] = data[i];
+                    literal_index += 1;
+                }
+                machines[literal_index] = '?'; 
+                literal_index += 1;
+            }
+            machines = machines[0..machines.len - 1];
         } else if (x == 1) {
             criterias = data;
         }
         x += 1;
     }
     const criterias_final = criteriasMaker(criterias);
-    var counter: i32 = 0;
-    try possibleCase(try criterias_final, machines, 0, 0, &counter);
+    std.debug.print("{s} {any}\n", .{machines, criterias_final});
+    const counter: i32 = 0;
     return counter;
 }
 fn array_contains(comptime T: type, haystack: []const T, needle: T) bool {
@@ -55,139 +67,6 @@ fn checkValidate(machines: []const u8) !bool {
     return true;
 }
 
-fn possibleCase(criterias: []u8, current_machine: []const u8, index_criteria: u8, index_machine: i8, counter: *i32) !void {
-    const index_parse: u8 = @bitCast(index_machine);
-    if (index_parse >= current_machine.len) {
-        return;
-    }
-    var list_check = ArrayList(i8).init(std.heap.page_allocator);
-    defer list_check.deinit();
-
-    for (index_parse..current_machine.len) |track_index| {
-        var pass_: bool = true;
-        const track_index_parse: i8 = @intCast(track_index);
-        const pos = try move(track_index_parse, criterias[index_criteria], current_machine);
-        if (index_criteria == criterias.len -% 1 and pos != -1) {
-            const pos_parse: u8 = @bitCast(pos);
-            if (array_contains(i8, list_check.items, pos) == false) {
-                const pos_parse_check: u8 = @bitCast(pos - 1);
-                pass_ = try checkValidate(current_machine[index_parse .. pos_parse_check -% criterias[index_criteria]]);
-                if (pass_ == true) {
-                    if (pos_parse < current_machine.len) {
-                        const chunks = try checkValidate(current_machine[pos_parse..current_machine.len]);
-                        if (chunks == true) {
-                            try list_check.append(pos);
-                            counter.* += 1;
-                        }
-                    } else {
-                        try list_check.append(pos);
-                        counter.* += 1;
-                    }
-                }
-            }
-        } else if (pos != -1) {
-            if (array_contains(i8, list_check.items, pos) == false) {
-                const pos_parse: u8 = @bitCast(pos - 1);
-
-                pass_ = try checkValidate(current_machine[index_parse .. pos_parse -% criterias[index_criteria]]);
-                if (pass_ == true) {
-                    try list_check.append(pos);
-                    try possibleCase(criterias, current_machine, index_criteria +% 1, pos, counter);
-                }
-            }
-        }
-    }
-    return;
-}
-
-fn move(position: i8, chars: u8, line: []const u8) !i8 {
-    var pos_check: u8 = @bitCast(position);
-    if (pos_check >= line.len) {
-        return -1;
-    }
-    while (line[pos_check] == '.') {
-        if (pos_check == line.len - 1) {
-            return -1;
-        }
-        pos_check += 1;
-    }
-
-    var counter_check: u8 = 0;
-    // move backward
-    if (line[pos_check] != '.' and pos_check > 0) {
-        if (line[pos_check -% 1] == '#') {
-            pos_check -%= 1;
-            while (pos_check != 255) {
-                if (line[pos_check] == '#') {
-                    pos_check -%= 1;
-                } else {
-                    break;
-                }
-            }
-            if (pos_check == 255) {
-                pos_check = 0;
-            } else {
-                pos_check +%= 1;
-            }
-        }
-    }
-
-    if (pos_check +% chars - 1 >= line.len) {
-        return -1;
-    }
-
-    var pass_hash = false;
-    while (counter_check != chars) : (pos_check += 1) {
-        if (pos_check == line.len) {
-            break;
-        }
-        if (line[pos_check] == '#') {
-            pass_hash = true;
-        }
-
-        if (line[pos_check] == '.' and pass_hash == false) {
-            counter_check = 0;
-        } else if (line[pos_check] == '.' and pass_hash == true) {
-            return -1;
-        } else {
-            counter_check += 1;
-        }
-    }
-    if (counter_check == chars) {
-        if (pos_check < line.len) {
-            if (line[pos_check] == '#') {
-                if (counter_check == 1) {
-                    return -1;
-                }
-                if (pos_check + 1 >= line.len) {
-                    return @intCast(line.len +% 1);
-                } else {
-                    var counter_hash: u8 = 0;
-                    pos_check += 1;
-                    while (pos_check < line.len) : (pos_check += 1) {
-                        if (line[pos_check] == '#') {
-                            counter_hash += 1;
-                        } else if (line[pos_check] == '.' or line[pos_check] == '?') {
-                            break;
-                        }
-                    }
-                    if (counter_hash <= chars and pos_check + 1 <= line.len + 1) {
-                        return @intCast(pos_check + 1);
-                    } else {
-                        return -1;
-                    }
-                }
-            }
-        }
-        if (pos_check + 1 > line.len + 1) {
-            return -1;
-        } else {
-            return @intCast(pos_check + 1);
-        }
-    }
-    return -1;
-}
-
 fn criteriasMaker(criterias: []const u8) ![]u8 {
     var allocator = std.heap.page_allocator;
     var it = std.mem.split(u8, criterias, ",");
@@ -195,14 +74,17 @@ fn criteriasMaker(criterias: []const u8) ![]u8 {
     while (it.next() != null) {
         len += 1;
     }
-    var result = try allocator.alloc(u8, len);
+    var result = try allocator.alloc(u8, len * 5);
     it.index = 0;
     // Fill the result array with parsed integers
     var index: usize = 0;
-    while (it.next()) |token| {
-        const maybe_num = try std.fmt.parseInt(u8, token, 10);
-        result[index] = maybe_num;
-        index += 1;
+    for (0..5) |_| {
+        while (it.next()) |token| {
+            const maybe_num = try std.fmt.parseInt(u8, token, 10);
+            result[index] = maybe_num;
+            index += 1;
+        }
+        it.index = 0;
     }
     return result;
 }
