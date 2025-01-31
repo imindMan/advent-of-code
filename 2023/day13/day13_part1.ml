9bfa6c5c0c472c128983d8d6628d88834f6bc75c
imindMan <khanhdonglethanhtong7b2@gmail.com>
2025-01-31 17:10:52 +0700

    Tempting 2

--- 2023/day13/day13_part1.ml
+++ 2023/day13/day13_part1.ml
@@ -11,7 +11,18 @@ let rec maps map_sep index =
   else 
     let (s, i) = group_one_map map_sep index in
     s :: (maps map_sep (i + 1))
-  
+ 
+let transpose map = 
+  for i = 0 to (Array.length map) - 1 do
+    for j = 0 to (Bytes.length map.(0)) - 1 do
+      printf "%d %d\n" i j;
+      (*swap things*)
+      let k = Bytes.get map.(i) j in
+      Bytes.set map.(i) j (Bytes.get map.(j) i);
+      Bytes.set map.(j) i k;
+    done;
+  done;;
+
 (*this is for reading the file*)
 let read_whole_chan chan =
   let buf = Buffer.create 4096 in
@@ -31,6 +42,7 @@ let read_whole_file filename =
 
 (*main part of the program. don't know why i cant use main ()*)
 let () =
-  let str = read_whole_file "day13.txt" in
-    let l = str |> String.split_on_char '\n' |> Array.of_list in
-      List.iter (printf "'%s',\n") (List.map (fun x -> String.sub x 0 ((String.length x) - 1)) (maps l 0));
+  let str = read_whole_file "day13.txt" in 
+  let l = str |> String.split_on_char '\n' |> Array.of_list in
+  let final_maps = List.map (fun x -> String.sub x 0 ((String.length x) - 1)) (maps l 0)  in
+    List.iter (fun x -> let x_arr = (Array.map (Bytes.of_string) (Array.of_list (x |> String.split_on_char '\n'))) in transpose x_arr; Array.iter (fun y -> printf "'%s',\n" (Bytes.to_string y)) x_arr) final_maps
