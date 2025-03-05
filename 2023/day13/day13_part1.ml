85d9e439e32d0a681f37bea98f699c450aef948d
imindMan <khanhdonglethanhtong7b2@gmail.com>
2025-03-05 22:05:31 +0700

    Save the state for nothing

--- 2023/day13/day13_part1.ml
+++ 2023/day13/day13_part1.ml
@@ -5,43 +5,44 @@ let read_whole_chan chan =
   let buf = Buffer.create 4096 in
   try
     while true do
-     let line = input_line chan in
-        Buffer.add_string buf line;
-        Buffer.add_char buf '\n'
-    done;
-    assert false 
-  with
-    End_of_file -> Buffer.contents buf
+      let line = input_line chan in
+      Buffer.add_string buf line ; Buffer.add_char buf '\n'
+    done ;
+    assert false
+  with End_of_file -> Buffer.contents buf
 
 let read_whole_file filename =
   let chan = open_in filename in
-    read_whole_chan chan
+  read_whole_chan chan
 
 (*this is for parsing the maps*)
 let rec group_one_map map_sep index =
   match String.length map_sep.(index) with
-   | 0 -> ("", index)
-   | _ -> let (s, i) = group_one_map map_sep (index + 1) in (map_sep.(index) ^ "\n" ^ s, i)
+  | 0 ->
+      ("", index)
+  | _ ->
+      let s, i = group_one_map map_sep (index + 1) in
+      (map_sep.(index) ^ "\n" ^ s, i)
 
 let rec maps map_sep index =
-  if index >= (Array.length map_sep) then []
-  else 
-    let (s, i) = group_one_map map_sep index in
-    s :: (maps map_sep (i + 1))
+  if index >= Array.length map_sep then []
+  else
+    let s, i = group_one_map map_sep index in
+    s :: maps map_sep (i + 1)
 
-let convert_map map = 
-  let new_map = Array.of_list (map |> String.split_on_char '\n') in new_map
+let convert_map map =
+  let new_map = Array.of_list (map |> String.split_on_char '\n') in
+  new_map
 
-let transpose (matrix: string array) : string array =
+let transpose (matrix : string array) : string array =
   let rows = Array.length matrix in
   if rows = 0 then [||]
   else
     let cols = String.length matrix.(0) in
     Array.init cols (fun i ->
-      let new_row = Bytes.create rows in
-      Array.iteri (fun j row -> Bytes.set new_row j (String.get row i)) matrix;
-      Bytes.to_string new_row
-    )
+        let new_row = Bytes.create rows in
+        Array.iteri (fun j row -> Bytes.set new_row j (String.get row i)) matrix ;
+        Bytes.to_string new_row )
 
 (*logic may be here*)
 let rec chop_last = function
@@ -52,10 +53,18 @@ let rec chop_last = function
   | x :: xs ->
       x :: chop_last xs
 
-let perfect_symmetry (arr : string array) list_holding const_index: int * int =
+let perfect_symmetry (arr : string array) list_holding const_index : int * int =
   let rec check pre_change const_index pass_it =
-    let i = if pass_it = false then if const_index = 0 then const_index else pre_change else pre_change in
-    let j = if pass_it = false then if const_index <> 0 then const_index else pre_change else const_index in
+    let i =
+      if pass_it = false then
+        if const_index = 0 then const_index else pre_change
+      else pre_change
+    in
+    let j =
+      if pass_it = false then
+        if const_index <> 0 then const_index else pre_change
+      else const_index
+    in
     if i > j then (i, j) (* Base case: the entire array has been checked *)
     else if arr.(i) = arr.(j) && i <> j then check (i + 1) (j - 1) true
       (* Move towards the center *)
@@ -88,18 +97,20 @@ let symmetry (arr : string array) =
   in
   if head_result <> (-1, -1) then head_result else tail_result
 
-
 let analyze (map : string) =
   let rows = symmetry (convert_map map) in
   let cols = symmetry (transpose (convert_map map)) in
-  if rows <> (-1, -1) && cols <> (-1, -1) then 100 * (snd rows + 1) + (snd cols + 1)
-  else if rows <> (-1, -1) then 100*(snd rows + 1)
+  if rows <> (-1, -1) && cols <> (-1, -1) then
+    (100 * (snd rows + 1)) + (snd cols + 1)
+  else if rows <> (-1, -1) then 100 * (snd rows + 1)
   else snd cols + 1
 
 (*main part of the program*)
 let () =
-  let str = read_whole_file "day13.txt" in 
+  let str = read_whole_file "day13.txt" in
   let l = str |> String.split_on_char '\n' |> Array.of_list in
-  let final_maps = List.map (fun x -> String.sub x 0 ((String.length x) - 1)) (maps l 0) in
-  let res = List.fold_left (fun acc x -> acc + (analyze x)) 0 final_maps in
-  printf "%d\n" res;
+  let final_maps =
+    List.map (fun x -> String.sub x 0 (String.length x - 1)) (maps l 0)
+  in
+  let res = List.fold_left (fun acc x -> acc + analyze x) 0 final_maps in
+  printf "%d\n" res
